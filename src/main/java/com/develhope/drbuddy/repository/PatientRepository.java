33d699e135a6dc3b1f642e45d9f7eba56534424a
salvatoremancuso2003 <salvatoremancuso703@gmail.com>
2023-03-06 13:47:11 +0100

    Repositories-feature

--- src/main/java/com/develhope/drbuddy/repository/PatientRepository.java
+++ src/main/java/com/develhope/drbuddy/repository/PatientRepository.java
@@ -1,4 +1,10 @@
 package com.develhope.drbuddy.repository;
 
-public class PatientRepository {
+import com.develhope.drbuddy.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+
 }
--- src/main/java/com/develhope/drbuddy/repository/ReservationRepository.java
+++ src/main/java/com/develhope/drbuddy/repository/ReservationRepository.java
@@ -1,4 +1,13 @@
 package com.develhope.drbuddy.repository;
 
-public class ReservationRepository {
+import com.develhope.drbuddy.entities.Doctor;
+import com.develhope.drbuddy.entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface ReservationRepository extends JpaRepository <Reservation, Integer> {
+    List<Reservation> findByDoctor (Doctor doctor_id);
+    List<Reservation> findByPatient (Doctor patient_id);
 }
