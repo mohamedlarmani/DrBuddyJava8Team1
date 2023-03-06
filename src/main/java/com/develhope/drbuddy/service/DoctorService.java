42d73158a07eb71ab73d808533813810a5dcd6b0
E1thar <105929315+E1thar@users.noreply.github.com>
2023-03-06 13:46:24 +0100

    Added Service class

--- src/main/java/com/develhope/drbuddy/service/DoctorService.java
+++ src/main/java/com/develhope/drbuddy/service/DoctorService.java
@@ -1,4 +1,30 @@
 package com.develhope.drbuddy.service;
 
+import com.develhope.drbuddy.entities.Doctor;
+import com.develhope.drbuddy.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
 public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public Doctor saveDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    public void deleteDoctor(int doctorId) {
+        doctorRepository.deleteById(doctorId);
+    }
+
+    public Optional<Doctor> getDoctorById(int doctorId){
+        return Optional.of(doctorRepository.getById(doctorId));
+    }
+
+
 }
--- src/main/java/com/develhope/drbuddy/service/PatientService.java
+++ src/main/java/com/develhope/drbuddy/service/PatientService.java
@@ -1,4 +1,29 @@
 package com.develhope.drbuddy.service;
 
+import com.develhope.drbuddy.entities.Patient;
+import com.develhope.drbuddy.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
 public class PatientService {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    public void deletePatient(int patientId) {
+        patientRepository.deleteById(patientId);
+    }
+
+    public Optional<Patient> getPatientById(int patientId){
+        return Optional.of(patientRepository.getById(patientId));
+    }
+
 }
