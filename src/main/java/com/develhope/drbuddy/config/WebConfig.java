adfeec7dd255f3fea2cd9fc63d49c5c8b30fd2fd
E1thar <105929315+E1thar@users.noreply.github.com>
2023-04-23 15:18:22 +0200

    Modified class WebConfig

--- src/main/java/com/develhope/drbuddy/config/WebConfig.java
+++ src/main/java/com/develhope/drbuddy/config/WebConfig.java
@@ -11,7 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .exposedHeaders("Content-Type", "Content-Length", "Host")
+                .allowCredentials(false)
+                .maxAge(3600);
     }
 }
 
